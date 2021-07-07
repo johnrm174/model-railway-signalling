f07b3e82606f931e4ec5ecd86eaa3aaf4043b744
johnrm174 <johnrm174@btinternet.com>
2021-07-07 14:57:12 +0100

    Formal release 1.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="model-railway-signals",
-    version="1.0.1",
+    version="1.1.0",
     description="Create your own DCC model railway signalling scheme",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -25,6 +25,6 @@ setup(
     ],
     packages=["model_railway_signals"],
     include_package_data=True,
-    install_requires=["pyserial", ""]
+    install_requires=["pyserial"]
 
 )
