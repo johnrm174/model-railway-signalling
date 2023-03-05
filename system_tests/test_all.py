2157e30df4c5f8c025d312038cfe920e1d6091ca
johnrm174 <johnrm174@btinternet.com>
2023-03-05 16:48:55 +0000

    updated to include schematic tests

--- system_tests/test_all.py
+++ system_tests/test_all.py
@@ -4,8 +4,10 @@
 
 import test_interlocking_examples
 import test_automation_examples
+import test_schematic_editor
 import system_test_harness
 
+test_schematic_editor.run_all_schematic_editor_tests()
 test_interlocking_examples.run_all_interlocking_example_tests()
 test_automation_examples.run_all_automation_example_tests()
 system_test_harness.complete_tests(shutdown=True)
