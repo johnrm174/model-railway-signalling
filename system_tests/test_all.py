1e91d8be05fbfb2369e368cfa8d98d3ba3d0a779
johnrm174 <johnrm174@btinternet.com>
2023-05-26 09:32:46 +0100

    added negative tests for layout load

--- system_tests/test_all.py
+++ system_tests/test_all.py
@@ -8,10 +8,12 @@ import test_schematic_editor
 import test_object_editing
 import test_single_line_examples
 import system_test_harness
+import load_layout_negative_tests
 
 test_schematic_editor.run_all_schematic_editor_tests()
 test_object_editing.run_all_object_editing_tests()
 test_interlocking_examples.run_all_interlocking_example_tests()
 test_automation_examples.run_all_automation_example_tests()
 test_single_line_examples.run_all_single_line_example_tests()
+load_layout_negative_tests.run_all_load_layout_negative_tests()
 system_test_harness.complete_tests(shutdown=True)
