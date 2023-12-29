7dff209ae7d3410149d03525cf720e7a9d9b40ae
johnrm174 <johnrm174@btinternet.com>
2023-12-29 16:38:31 +0000

    tidy up scripts

--- scripts/signalling.sh
+++ scripts/signalling.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 # signalling.sh
 #
-# Example shell script to run up the model railway signsalling application
-# And write the logs to a log file based on the current time and date
-# Permissions should be set to make the script executable: chmod 755 signalling.sh
+# Example shell script to run up the model railway signalling application
+# Will also re-direct the logs to a file based on the current time and date
 # Script should be saved as: /home/pi/.config/autostart/signalling.sh
+# Permissions should be set to make the script executable: chmod 755 signalling.sh
 #
 filename=signalling.log
 current_time=$(date "+%Y%m%d-%H%M%S")
 logfilename=$current_time-$filename
+echo $logfilename >> /home/pi/.config/autostart/$logfilename 2>&1
 #
-# you can modify the following to use the -f <filename> option to load a layout on startup
+# Modify the following to use the -f <filename> option to load a layout on startup
 # eg: python3 -m model_railway_signals -f /home/pi/my_layout.sig
 #
-echo $logfilename >> /home/pi/.config/autostart/$logfilename 2>&1
 python3 -m model_railway_signals >> /home/pi/.config/autostart/$logfilename 2>&1
 #
 # Uncomment the following lines to shut down the Raspberry Pi on application exit
-# Useful for remote nodes being used to publish networked track sensors
+# Useful for when remote nodes are being used to publish networked track sensors
+# Note we give ourselves a minute before shutdown to intervene if things go wrong
 #
 #echo "Shutting down in 60 seconds" >> /home/pi/.config/autostart/$logfilename 2>&1
 #sleep 60
