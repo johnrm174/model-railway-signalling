bb238b400e6e380b806f134495d0d4e64c55e7a9
johnrm174 <johnrm174@btinternet.com>
2024-11-29 09:14:17 +0000

    remove duplicate params from button creation and update inline comments to clarify why we are using zfill to set the placeholder width

--- model_railway_signals/library/buttons.py
+++ model_railway_signals/library/buttons.py
@@ -250,18 +250,19 @@ def create_button (canvas, button_id:int, buttontype:button_type, x:int, y:int, 
         logging.debug("Button "+str(button_id)+": Creating Button on the Canvas")
         # Create the Button and its canvas window (for Run Mode operation). Note the Window is created
         # as 'hidden' - assuming we are in edit mode - but changed later if we are in Run Mode
-        button = Tk.Button(canvas, text=label, state="normal", relief="raised", width=width, foreground="grey40",
-                        font=font, bg=button_colour, activebackground=active_colour, padx=2, pady=2,
-                        activeforeground=text_colour, fg=text_colour,  command=lambda:button_event(button_id))
+        button = Tk.Button(canvas, text=label, state="normal", relief="raised", width=width,
+                        font=font, background=button_colour, activebackground=active_colour, padx=2, pady=2,
+                        activeforeground=text_colour, foreground=text_colour, command=lambda:button_event(button_id))
         button_window = canvas.create_window(x, y, window=button, tags=canvas_tag, state="hidden")
         # Create and store a tool-tip for the button
         tooltip_object = CreateToolTip(button, text=tooltip)
         tooltip_object.waittime = 200     # miliseconds
         tooltip_object.wraplength = 400   # pixels
-        # Create the 'placeholder' for the button to display in Edit Mode (so it an be selected/moved)
-        # Note that the 'width' parameter is the maximum width in pixels before the text starts to wrap. To set the
-        # minimum width we need to specify an initial 'text' value that contains the required number of characters.
-        # The font size is the  2nd parameter of the 'font' tuple
+        # Create the 'placeholder' for the button to display in Edit Mode (so it an be selected/moved).
+        # Note that the 'width' parameter for the canvas.create_text function is the maximum width in pixels
+        # before the text starts to wrap so we can't use this to set the minimum width of the placeholded object.
+        # Instead, we need to specify an initial 'text' value that contains the required number of characters
+        # (using zfill) and change this later.
         placeholder1 = canvas.create_text(x, y, text="".zfill(width), font=font, fill=text_colour, tags=canvas_tag)
         bbox = canvas.bbox(placeholder1)
         placeholder2 = canvas.create_rectangle(bbox[0]-4, bbox[1]-4, bbox[2]+4, bbox[3]+2,
