2c206893653481bbc2e3f42c377df1b2055682f6
johnrm174 <johnrm174@btinternet.com>
2024-12-11 10:43:20 +0000

    tidy up code for consistency

--- model_railway_signals/editor/objects/objects_sections.py
+++ model_railway_signals/editor/objects/objects_sections.py
@@ -147,7 +147,7 @@ def redraw_section_object(object_id):
                 section_width = objects_common.schematic_objects[object_id]["buttonwidth"],
                 mirror_id = objects_common.schematic_objects[object_id]["mirror"],
                 font = objects_common.schematic_objects[object_id]["textfonttuple"],
-                button_colour = objects_common.schematic_objects[object_id]["buttoncolour"],
+                button_colour = button_colour,
                 text_colour = text_colour)
     # Create/update the canvas "tags" and selection rectangle for the Track Section
     objects_common.schematic_objects[object_id]["tags"] = canvas_tags
@@ -218,14 +218,15 @@ def update_section_styles(object_id, dict_of_new_styles:dict):
     # The text_colour is set according to the text colour type and background colour
     button_colour = objects_common.schematic_objects[object_id]["buttoncolour"]
     text_colour_type = objects_common.schematic_objects[object_id]["textcolourtype"]
+    text_colour = objects_common.get_text_colour(text_colour_type, button_colour)
     # Update the styles of the library object
     track_sections.update_section_styles(
             section_id = objects_common.schematic_objects[object_id]["itemid"],
             default_label = objects_common.schematic_objects[object_id]["defaultlabel"],
             section_width = objects_common.schematic_objects[object_id]["buttonwidth"],
-            button_colour = objects_common.schematic_objects[object_id]["buttoncolour"],
-            text_colour = objects_common.get_text_colour(text_colour_type, button_colour),
-            font = objects_common.schematic_objects[object_id]["textfonttuple"] )
+            font = objects_common.schematic_objects[object_id]["textfonttuple"],
+            button_colour = button_colour,
+            text_colour = text_colour)
     # Create/update the selection rectangle for the Track Section
     objects_common.set_bbox(object_id, objects_common.schematic_objects[object_id]["tags"])
     return()
