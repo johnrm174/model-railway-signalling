542c1d67a22fd4b79dd09c1024db9cb2587fd3e5
johnrm174 <johnrm174@btinternet.com>
2026-01-08 09:32:56 +0000

    stop tests if library tests fail

--- system_tests/test_all.py
+++ system_tests/test_all.py
@@ -31,39 +31,42 @@ def run_all_tests():
     basic_library_tests2.run_all_basic_library_tests()
     print("*** Running tests from 'basic_library_tests3.py' ***")
     basic_library_tests3.run_all_basic_library_tests()
-    
-    print("*** Running tests from 'test_menubar_windows.py' ***")
-    test_menubar_windows.run_all_menubar_window_tests()
-    print("*** Running tests from 'test_configuration_windows.py' ***")
-    test_configuration_windows.run_all_configuration_window_tests()
-    
-    print("*** Running tests from 'test_settings_updates.py' ***")
-    test_settings_updates.run_all_settings_update_tests()
-
-    print("*** Running tests from 'test_schematic_editor.py' ***")
-    test_schematic_editor.run_all_schematic_editor_tests()
-    print("*** Running tests from 'test_object_updates.py' ***")
-    test_object_updates.run_all_object_editing_tests()
-    print("*** Running tests from 'test_run_layout.py' ***")
-    test_run_layout.run_all_run_layout_tests()
-    print("*** Running tests from 'test_mqtt_networking.py' ***")
-    test_mqtt_networking.run_all_mqtt_networking_tests()
-    print("*** Running tests from 'test_schematic_routes.py' ***")
-    test_schematic_routes.run_all_schematic_routes_tests()
-    print("*** Running tests from 'test_signalbox_levers.py' ***")
-    test_signalbox_levers.run_all_signalbox_lever_tests()
-
-    print("*** Running tests from 'test_automation_examples.py' ***")
-    test_automation_examples.run_all_automation_example_tests()
-    print("*** Running tests from 'test_single_line_examples.py' ***")
-    test_single_line_examples.run_all_single_line_example_tests()
-    print("*** Running tests from 'test_mqtt_networking_example.py' ***")
-    test_mqtt_networking_example.run_all_mqtt_networking_example_tests()
-
-    print("*** Running tests from 'test_load_layout_failures.py' ***")
-    test_load_layout_failures.run_all_load_layout_negative_tests()
-
     system_test_harness.report_results()
+    
+    if system_test_harness.test_failures == 0:
+
+        print("*** Running tests from 'test_menubar_windows.py' ***")
+        test_menubar_windows.run_all_menubar_window_tests()
+        print("*** Running tests from 'test_configuration_windows.py' ***")
+        test_configuration_windows.run_all_configuration_window_tests()
+        
+        print("*** Running tests from 'test_settings_updates.py' ***")
+        test_settings_updates.run_all_settings_update_tests()
+
+        print("*** Running tests from 'test_schematic_editor.py' ***")
+        test_schematic_editor.run_all_schematic_editor_tests()
+        print("*** Running tests from 'test_object_updates.py' ***")
+        test_object_updates.run_all_object_editing_tests()
+        print("*** Running tests from 'test_run_layout.py' ***")
+        test_run_layout.run_all_run_layout_tests()
+        print("*** Running tests from 'test_mqtt_networking.py' ***")
+        test_mqtt_networking.run_all_mqtt_networking_tests()
+        print("*** Running tests from 'test_schematic_routes.py' ***")
+        test_schematic_routes.run_all_schematic_routes_tests()
+        print("*** Running tests from 'test_signalbox_levers.py' ***")
+        test_signalbox_levers.run_all_signalbox_lever_tests()
+
+        print("*** Running tests from 'test_automation_examples.py' ***")
+        test_automation_examples.run_all_automation_example_tests()
+        print("*** Running tests from 'test_single_line_examples.py' ***")
+        test_single_line_examples.run_all_single_line_example_tests()
+        print("*** Running tests from 'test_mqtt_networking_example.py' ***")
+        test_mqtt_networking_example.run_all_mqtt_networking_example_tests()
+
+        print("*** Running tests from 'test_load_layout_failures.py' ***")
+        test_load_layout_failures.run_all_load_layout_negative_tests()
+
+        system_test_harness.report_results()
 
 # The main code starts here
 system_test_harness.start_application(run_all_tests)
